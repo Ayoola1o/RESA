39c2389dd828f83ea909c653a4418901e5ff383f
Ayoola Adebisi <ayoolaadebisi5@gmail.com>
2025-08-13 08:47:08 +0000

    4th commit postcss packages update3

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,7 +2,7 @@ import tailwindcss from '@tailwindcss/postcss'
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    [tailwindcss]: {},
+    tailwindcss: {},
   },
 };
 
