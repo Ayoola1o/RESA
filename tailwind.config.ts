d3d813dd23918b233b89ef2d07c84cce6ed2003a
Ayoola Adebisi <ayoolaadebisi5@gmail.com>
2025-08-14 07:48:08 +0000

    ok do that

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,11 +3,6 @@ import tailwindcssAnimate from 'tailwindcss-animate';
 
 const config = {
   darkMode: ['class'],
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
   theme: {
     container: {
       center: true,
