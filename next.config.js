6e5e6a49eb8fe5fe6840e93f864ce4c00a53cf0a
Ayoola1o <31098295+Ayoola1o@users.noreply.github.com>
2025-08-18 11:16:19 +0000

    push from github

---
+++ next.config.js
@@ -0,0 +1,21 @@
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
